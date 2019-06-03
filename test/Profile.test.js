12a986f2518a344c0a42959d39fc59a2f0e47a26
sepuckett86 <sepuckett86@gmail.com>
2019-06-03 16:19:09 -0700

    add Profile test for DDD

---
+++ test/Profile.test.js
@@ -0,0 +1,28 @@
+import Profile from '../src/shared/Profile.js';
+
+const test = QUnit.test;
+
+QUnit.module('Profile');
+
+test('renders profile html', assert => {
+    // Arrange
+    const user = {
+        displayName: 'Static Name',
+        photoURL: './assets/blank-profile-picture.png'
+    };
+    const expected = /*html*/ `
+        <div id="profile">
+            <img id="profile-image" src="./assets/blank-profile-picture.png">
+            <div id="profile-info">
+                <span>Name</span>
+                <button>Log Out</button>
+            </div>
+        </div>
+    `;
+    // Act
+    const profile = new Profile({ user });
+    const actual = profile.renderTemplate();
+
+    // Assert
+    assert.htmlEqual(actual, expected);
+});
--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,7 @@
 import './html-equal.js';
+
 import './hash-storage.test.js';
 import './html-equal.test.js';
 import './html-to-DOM.test.js';
+
+import './Profile.test.js';
