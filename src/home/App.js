18caf7a31ddd64a23307d3113478241b9778654b
sepuckett86 <sepuckett86@gmail.com>
2019-06-03 15:58:28 -0700

    add header and wire auth

--- src/home/App.js
+++ src/home/App.js
@@ -1,6 +1,17 @@
 import Component from '../Component.js';
+import Header from '../shared/Header.js';
 
 class App extends Component {
+    render() {
+        const dom = this.renderDOM();
+
+        const header = new Header();
+        const headerDOM = header.render();
+
+        dom.prepend(headerDOM);
+
+        return dom;
+    }
     renderTemplate() {
         return /*html*/ `
             <div>
