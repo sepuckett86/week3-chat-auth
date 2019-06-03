b6f006f3974d2c144c4bee09864570ced2887066
sepuckett86 <sepuckett86@gmail.com>
2019-06-03 15:48:13 -0700

    set up basic files

---
+++ src/home/App.js
@@ -0,0 +1,14 @@
+import Component from '../Component.js';
+
+class App extends Component {
+    renderTemplate() {
+        return /*html*/ `
+            <div>
+                <main>
+                </main>
+            </div>
+        `;
+    }
+}
+
+export default App;
