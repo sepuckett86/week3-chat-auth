6ec976b065a88f18653777af1e7fedefd9902742
sepuckett86 <sepuckett86@gmail.com>
2019-06-05 11:12:34 -0700

    add styling

--- src/chat/ChatItem.js
+++ src/chat/ChatItem.js
@@ -6,7 +6,7 @@ class ChatItem extends Component {
             <li>
                 <div class="chat-info">
                     <img class="chat-pic" src="./assets/blank-profile-picture.png">
-                    <span>Name</span>
+                    <span>Name Here Super</span>
                 </div>
                 <p>Chat message goes here. It's really cool!</p>
             </li>
